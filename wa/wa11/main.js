356a9c29137d8be9bd59ed3c5d7424eac0a45d81
so7aki <soan3760@colorado.edu>
2024-04-04 13:24:22 -0600

    wa11

---
+++ wa/wa11/main.js
@@ -0,0 +1,47 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+
+const imageFilename = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+
+imageFilename.forEach(filename => {
+    
+    const newImage = document.createElement('img');
+    
+    
+    newImage.src = `images/${filename}`;
+    newImage.alt = filename;
+    
+   
+    thumbBar.appendChild(newImage);
+    
+    newImage.addEventListener('click', function() {
+        displayedImage.src = this.src; 
+        displayedImage.alt = this.alt; 
+    });
+});
+
+
+btn.addEventListener('click', function() {
+    const currentClass = btn.getAttribute('class');
+    if (currentClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
+});
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+
